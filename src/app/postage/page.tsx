f1e2bb403a4fcd600b0ca48e0115556a434119d6
Cuộc Sống ở Australia <harryhung195@gmail.com>
2025-04-14 10:33:25 +0000

    can you design Postage & Delivery

---
+++ src/app/postage/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Postage() {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-bold mb-4">Postage &amp; Delivery Information</h1>
+      <p>This page will contain details about our postage and delivery options, including shipping rates, delivery times, and policies.</p>
+    </div>
+  );
+}
