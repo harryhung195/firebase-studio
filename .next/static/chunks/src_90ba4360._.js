e4f7478f0d10faa15153bff9f26ce9dc142c6736
Cuộc Sống ở Australia <harryhung195@gmail.com>
2025-04-19 04:16:40 +0000

    can you create total price in checkout

--- .next/static/chunks/src_90ba4360._.js
+++ .next/static/chunks/src_90ba4360._.js
@@ -151,6 +151,20 @@ function AdminPanel() {
                         fileName: "[project]/src/app/admin/page.tsx",
                         lineNumber: 15,
                         columnNumber: 9
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
+                        href: "/admin/users",
+                        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$ui$2f$button$2e$tsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["Button"], {
+                            children: "Manage Users"
+                        }, void 0, false, {
+                            fileName: "[project]/src/app/admin/page.tsx",
+                            lineNumber: 19,
+                            columnNumber: 11
+                        }, this)
+                    }, void 0, false, {
+                        fileName: "[project]/src/app/admin/page.tsx",
+                        lineNumber: 18,
+                        columnNumber: 10
                     }, this)
                 ]
             }, void 0, true, {
---
+++ .next/static/chunks/src_app_admin_users_page_tsx_d527e06b._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_admin_users_page_tsx_d527e06b._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_admin_users_page_tsx_d527e06b._.js",
+  "chunks": [
+    "static/chunks/node_modules_a31de8ea._.js",
+    "static/chunks/src_9785c6ac._.js"
+  ],
+  "source": "dynamic"
+});
